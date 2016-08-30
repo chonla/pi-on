33714de63f5fe4fb773d62afb16f64121d54a390
Chonlasith Jucksriporn <chonlasith@gmail.com>
2016-08-30 22:09:14 +0700

    fix img name, verify disk id

--- pi-on.sh
+++ pi-on.sh
@@ -17,23 +17,28 @@ fi
 
 DISKID=$1
 
+echo "Verifying disk ID"
+DISK="$(diskutil list | grep /dev/${DISKID})"
+if [ "${DISK}" == "" ]
+then
+    echo "==> /dev/${DISKID} is not accessible. Please ensure that ${DISKID} is mounted to your computer."
+    exit 1
+fi
+
 echo "Downloading archived image from ${URL}"
 FILE_SIZE="$(curl --head --silent -L $URL | grep -i content-length | awk '{print $2}' | tr -d '\r')"
 CHUNK_SIZE="$(expr ${FILE_SIZE} / ${CHUNK_COUNT})"
 CHUNK_MAX_INDEX="$(expr ${CHUNK_COUNT} - 1)"
-echo "==> Bytes to be downloaded: ${FILE_SIZE}"
 pids=""
+echo "==> Spawning concurrent downloads. This may take a few minutes. Be patient. :)"
 for i in `seq 0 ${CHUNK_MAX_INDEX}`;
 do
     FROM_BYTES="$(expr ${i} \* ${CHUNK_SIZE})"
     TO_BYTES="$(expr ${FROM_BYTES} + ${CHUNK_SIZE} - 1)"
-    echo "==> Initialize concurrent download #${i}."
     if [ $i -eq $CHUNK_MAX_INDEX ];
     then
-        echo "==> Download from byte ${FROM_BYTES} to the rest."
         curl --silent --range ${FROM_BYTES}- -o temp.part${i} -L ${URL} &
     else
-        echo "==> Download from byte ${FROM_BYTES} to ${TO_BYTES}."
         curl --silent --range ${FROM_BYTES}-${TO_BYTES} -o temp.part${i} -L ${URL} &
     fi
     pids="$pids $!"
@@ -46,13 +51,13 @@ RESOURCE="$(curl --silent ${BASE_URL})"
 SHA1="$(openssl sha1 temp.zip | awk '{print $2}')"
 if [[ $RESOURCE != *"${SHA1}"* ]]
 then
-    echo "Incorrect archive checksum. Operation aborted."
+    echo "==> Incorrect archive checksum. Operation aborted."
     exit -1
 fi
 
 echo "Unpacking archive"
 unzip -o temp.zip
-IMG_NAME="$(ls \*.img)"
+IMG_NAME="$(ls *.img)"
 
 echo "Cleaning up"
 rm temp.part?
@@ -62,7 +67,8 @@ echo "Preparing disk"
 diskutil eraseDisk FAT32 RASPBIAN MBRFormat /dev/${DISKID}
 diskutil unmountDisk ${DISKID}
 
-echo "Move image to disk"
+echo "Moving image to disk"
+echo "==> Writing image to disk may take some time. You may move your body for a good health."
 dd bs=1m if=./${IMG_NAME} of=/dev/r${DISKID}
 
-echo "Done"
+echo "Done. Thank you."
